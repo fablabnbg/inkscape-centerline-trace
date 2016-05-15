0ba557f636c26d2f0c7b477cea372938622a842f
Juergen Weigert <jw@owncloud.com>
2016-05-15 05:38:20 +0200

    build scripts update. names fixed, description added.

--- distribute/deb/dist.sh
+++ distribute/deb/dist.sh
@@ -3,14 +3,19 @@
 
 name=$1
 vers=$2
-out=$3
-test -z "$out" && out=.
+url=http://github.com/fablabnbg/inkscape-centerline-trace
+requires="bash, python3-PIL|python-PIL, autotrace"
 
-[ -d tmp ] && rm -rf tmp
-mkdir tmp
+tmp=../out
+
+[ -d $tmp ] && rm -rf $tmp/*.deb
+mkdir $tmp
 cd files
-fakeroot checkinstall --fstrans --reset-uid --type debian --install=no -y --pkgname $name --pkgversion $vers --arch all --pkglicense LGPL --pkggroup other --pkgsource "http://github.com/fablabnbg/inkscape-chain-paths" --pkgaltsource "http://fablab-nuernberg.de" --pakdir tmp/ --maintainer "'Juergen Weigert (juewei@fabmail.org)'" --requires "bash" make install -e PREFIX=/usr > /dev/null || { echo "error"; exit 1; }
-mkdir -p $out
-mv tmp/*.deb $out
-rm -rf tmp
+fakeroot checkinstall --fstrans --reset-uid --type debian \
+  --install=no -y --pkgname $name --pkgversion $vers --arch all \
+  --pkglicense LGPL --pkggroup other --pakdir ../$tmp --pkgsource $url \
+  --pkgaltsource "http://fablab-nuernberg.de" \
+  --maintainer "'Juergen Weigert (juewei@fabmail.org)'" \
+  --requires "$requires" make install \
+  -e PREFIX=/usr > /dev/null || { echo "error"; exit 1; }
 
--- distribute/distribute.sh
+++ distribute/distribute.sh
@@ -2,16 +2,17 @@
 echo "Determining Version:"
 VERSION=$(sed -ne 's@.*version\s\([a-z0-9\.]*\).*@\1@pi' ../centerline-trace.inx)
 echo "Version is: \"$VERSION\""
-name=centerline-trace
+name=inkscape-centerline-trace
 if [ -d $name ]
 then
 	echo "Removing leftover files"
 	rm -rf $name
 fi
-echo "Copying content..."
+echo "Copying contents ..."
 mkdir $name
 cp ../README.md $name/README
 cp ../LICENSE* $name/
+cp ../centerline-trace-poster.svg $name/
 cp ../*.py ../*.inx ../Makefile $name/
 
 
@@ -22,7 +23,8 @@ read answer
 if [ "$answer" != "n" ]
 then
   cp -a $name/* deb/files
-  (cd deb && sh ./dist.sh $name $VERSION ..)
+  (cd deb && sh ./dist.sh $name $VERSION)
 fi
 
 
+rm -rf $name
