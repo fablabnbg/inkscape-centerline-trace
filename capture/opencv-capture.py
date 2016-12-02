44fc5281b7394a2aea35c2c80765840ce19b5ab6
Juergen Weigert <jw@owncloud.com>
2016-12-02 16:22:20 +0100

    view finder added.

--- capture/opencv-capture.py
+++ capture/opencv-capture.py
@@ -1,10 +1,32 @@
 #! /usr/bin/python
 #
-# for cv2, cv3 see also http://stackoverflow.com/questions/18458422/query-maximum-resolution-in-opencv
-# http://stackoverflow.com/questions/7322939/how-to-count-cameras-in-opencv-2-3
+# References:
+# for cv2, cv3 see http://stackoverflow.com/questions/18458422/query-maximum-resolution-in-opencv
+# 		http://stackoverflow.com/questions/7322939/how-to-count-cameras-in-opencv-2-3
+# http://docs.opencv.org/modules/core/doc/drawing_functions.html
+# http://docs.opencv.org/modules/core/doc/operations_on_arrays.html?highlight=avg#mean
 #
 import os
 import cv
+
+prop = {
+  'bright':     [cv.CV_CAP_PROP_BRIGHTNESS],
+  'contr':      [cv.CV_CAP_PROP_CONTRAST],
+  'rgb':        [cv.CV_CAP_PROP_CONVERT_RGB],
+  'expose':     [cv.CV_CAP_PROP_EXPOSURE],
+  'format':     [cv.CV_CAP_PROP_FORMAT],
+  'fourcc':     [cv.CV_CAP_PROP_FOURCC],
+  'fps':        [cv.CV_CAP_PROP_FPS],
+  'count':      [cv.CV_CAP_PROP_FRAME_COUNT],
+  'height':     [cv.CV_CAP_PROP_FRAME_HEIGHT],
+  'width':      [cv.CV_CAP_PROP_FRAME_WIDTH],
+  'gain':       [cv.CV_CAP_PROP_GAIN],
+  'hue':        [cv.CV_CAP_PROP_HUE],
+  'mode':       [cv.CV_CAP_PROP_MODE],
+  'rect':       [cv.CV_CAP_PROP_RECTIFICATION],
+  'sat':        [cv.CV_CAP_PROP_SATURATION]
+  }
+
 
 camera_resolutions = (
   ( 4000, 3000 ),
@@ -43,20 +65,148 @@ if not cap:
   print "Error opening WebCAM"
   sys.exit(1)
 
+gui = { 'prop': {}, 'step': 8, 'nav': 'm' }
+
+idx = 0
+for p in prop:
+  x = cv.GetCaptureProperty(cap, prop[p][0])
+  if x >= 0:
+    idx += 1
+    prop[p].append(idx)        # property exists
+    prop[p].append(x)           # current value
+    prop[p].append(x)           # initial value
+  else:
+    prop[p].append(0)
+
+for p in prop:
+  if prop[p][1]:
+    print "property %s = '%s'" % (p, prop[p][2])
+gui['prop'] = prop;
+
+def draw_gui(cv, img):
+  if not 'w' in gui: gui['w'] = img.width
+  if not 'h' in gui: gui['h'] = img.height
+  if not 'frame' in gui:
+    frame = {
+      'w': int(img.width /2+.5),
+      'h': int(img.height/2+.5),
+      'x': int(img.width /4+.5),
+      'y': int(img.height/4+.5),
+      'c':(255,0,255)
+    }
+    gui['frame'] = frame
+  f = gui['frame']
+  ww=int(f['w']/8+.5)
+  hh=int(f['h']/8+.5)
+  col=f['c']
+
+  cv.Line(img, (f['x'],f['y']       ), (f['x']+ww,f['y']       ), col, 1)
+  cv.Line(img, (f['x'],f['y']       ), (f['x']   ,f['y']+hh    ), col, 1)
+
+  cv.Line(img, (f['x'],f['y']+f['h']), (f['x']+ww,f['y']+f['h']       ), col, 1)
+  cv.Line(img, (f['x'],f['y']+f['h']), (f['x']   ,f['y']+f['h']-hh    ), col, 1)
+
+  cv.Line(img, (f['x']+f['w'],f['y']+f['h']), (f['x']+f['w']-ww,f['y']+f['h']   ), col, 1)
+  cv.Line(img, (f['x']+f['w'],f['y']+f['h']), (f['x']+f['w']   ,f['y']+f['h']-hh), col, 1)
+
+  cv.Line(img, (f['x']+f['w'],f['y']), (f['x']+f['w']-ww,f['y']   ), col, 1)
+  cv.Line(img, (f['x']+f['w'],f['y']), (f['x']+f['w']   ,f['y']+hh), col, 1)
+
+def move_frame(dir):
+  f = gui['frame']
+  if dir == 1:
+    # left
+    f['x'] -= gui['step']
+    if f['x'] <= 0: f['x'] = 0
+  elif dir == 2:
+    # up
+    f['y'] -= gui['step']
+    if f['y'] <= 0: f['y'] = 0
+  elif dir == 3:
+    # right
+    f['x'] += gui['step']
+    if f['x']+f['w'] >= gui['w']: f['x'] = gui['w'] - f['w']
+  elif dir == 4:
+    # down
+    f['y'] += gui['step']
+    if f['y']+f['h'] >= gui['h']: f['y'] = gui['h'] - f['h']
+  else:
+    pass
+
+def scale_frame(dir):
+  f = gui['frame']
+  if dir == 1:
+    # left
+    f['w'] -= gui['step']
+    if f['w'] <= gui['step']: f['w'] = gui['step']
+  elif dir == 2:
+    # up
+    f['h'] -= gui['step']
+    if f['h'] <= gui['step']: f['h'] = gui['step']
+  elif dir == 3:
+    # right
+    f['w'] += gui['step']
+    if f['x']+f['w'] >= gui['w']: f['w'] = gui['w'] - f['x']
+  elif dir == 4:
+    # down
+    f['h'] += gui['step']
+    if f['y']+f['h'] >= gui['h']: f['h'] = gui['h'] - f['y']
+  else:
+    pass
+
+
+dropcount=0
 state=0
-while 1:
-  frame = cv.QueryFrame(cap)
-  if frame is None:
+while (True):
+  img = cv.QueryFrame(cap)
+  if img is None:
     break
-  cv.ShowImage('Camera Image', frame)
-  key = 0xFF & cv.WaitKey(1)
-  if key in ( 27, ord('q'), ord('Q'), ):
+  dropcount += 1
+  if (dropcount >= 2):
+    dropcount = 0
+  if (dropcount >= 1):
+    # drop every third image, so that we stay clear of 100% cpu consumption
+    # and keep the v4l buffers drained at low water. This counters excess latency.
+    continue
+
+  gray = cv.CreateImage( (img.width, img.height), cv.IPL_DEPTH_8U, 1 );
+  cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+  # cv.Canny(gray, gray, 50, 150, 3)	# Canny edge detector does double lines.
+  img = gray	# FIXME: expand to RGB again, so that we can draw a colorful GUI
+  draw_gui(cv, img)
+
+  cv.ShowImage('Camera Image', img)
+  key_raw = cv.WaitKey(1)
+  key = key_raw & 0xff    # force into ascii range
+
+  # 65364 84 = c_down
+  # 65362 82 = c_up
+  # 65361 81 = c_left
+  # 65363 83 = c_right
+  # 65513 233 = alt
+  # 65505 225 = shift
+
+  if (chr(key) == 'h'): key_raw = 65361
+  if (chr(key) == 'j'): key_raw = 65364
+  if (chr(key) == 'k'): key_raw = 65362
+  if (chr(key) == 'l'): key_raw = 65363
+
+  if key_raw in (65364, 65362, 65361, 65363):
+    if (gui['nav'] == 'm'):
+      move_frame(key_raw-65360)
+    else:
+      scale_frame(key_raw-65360)
+  elif key in ( 27, ord('q'), ord('Q'), ):
     break
-  if key in ( ord(' '), ord('w'), ord('W'), ord('S'), ord('s'), ):
+  elif key in ( ord(' '), ord('w'), ord('W'), ):
     state = 1
+  elif (chr(key) in "mrs"):
+    gui['nav'] = chr(key)
+
+
   if state == 1:
     # convert to grayscale
-    cv.SaveImage('pic.png', frame)
+    cv.SaveImage('pic.png', img)
     print("saving pic.png")
     state = 2
   elif state == 2:
